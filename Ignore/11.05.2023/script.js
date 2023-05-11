a4019c2cd7eef1a302c43268f04d32a989b250a5
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-05-11 10:57:14 +0300

    AAAAAAAAA

--- Ignore/11.05.2023/script.js
+++ Ignore/11.05.2023/script.js
@@ -1,14 +1,26 @@
 // Saglabā to checkbox JS mainīgajā
 const toggleTheme = document.getElementById("kaste");
 const html = document.getElementById("html");
-
 toggleTheme.addEventListener("change", toggleThemeHandler);
 
 function toggleThemeHandler() {
   console.log(toggleTheme.checked);
   if (toggleTheme.checked) {
     html.dataset.theme = "dark";
+    localStorage.setItem("theme", "dark");
   } else {
     html.dataset.theme = "light";
+    localStorage.setItem("theme", "light");
   }
 }
+
+const theme = localStorage.getItem("theme");
+
+if (theme == "light" || !theme) {
+  html.dataset.theme = "light";
+} else {
+  html.dataset.theme = "dark";
+  document.getElementById("kaste").checked = true;
+}
+
+toggleThemeHandler();
