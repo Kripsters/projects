8cf002bfc132ade5ee2397539be57d5e91e51404
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-05-18 10:50:17 +0300

    Pabeigts darbs

--- trip-quiz.js
+++ trip-quiz.js
@@ -2,7 +2,9 @@
 const toggleTheme = document.getElementById("kaste");
 const html = document.getElementById("html");
 const bilde = document.getElementById("imghead");
+const mybutton = document.getElementById("mybtn");
 toggleTheme.addEventListener("change", toggleThemeHandler);
+mybutton.addEventListener("click", checkAmountCorrect);
 
 function toggleThemeHandler() {
   console.log(toggleTheme.checked);
@@ -24,6 +26,30 @@ if (theme == "light" || !theme) {
 } else {
   html.dataset.theme = "dark";
   document.getElementById("kaste").checked = true;
+}
+
+function checkAmountCorrect() {
+  console.log("func checkAmountCorrect start");
+  var result = 0;
+  if (document.getElementById("answ1").checked == true) {
+    result = result + 1;
+  }
+  if (document.getElementById("answ3").checked == true) {
+    result = result + 1;
+  }
+  if (document.getElementById("answ4").checked == true) {
+    result = result + 1;
+  }
+  if (document.getElementById("answ2").checked == true) {
+    result = "Roma 💀";
+  }
+  if (document.getElementById("answ2").checked != true) {
+    document.getElementById("isCorrect").innerHTML =
+      "Pareizi izvelejies " + result + " no 3";
+  } else {
+    document.getElementById("isCorrect").innerHTML = result;
+  }
+  console.log("func checkAmountCorrect end");
 }
 
 toggleThemeHandler();
